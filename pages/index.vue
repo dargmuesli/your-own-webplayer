cd6d7200050b7de6dafd1973b7720f3b62921edb
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-09-07 22:43:35 +0200

    Fill Index Page

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,207 @@
   <div class="container mx-auto mb-4">
     <section>
       <h1>Player</h1>
+      <div class="flex flex-col mb-2">
+        <div class="bg-gray-900 flex-grow p-2">
+          <div class="m-auto w-5/6">
+            <h2 v-if="this.$route.query.playlist" class="ml-2">
+              {{ this.$route.query.playlist }}
+            </h2>
+            <ul
+              v-if="playlists !== undefined && playlists.length > 0"
+              class="flex flex-wrap justify-center list-none"
+            >
+              <li
+                v-for="playlist in playlists"
+                :key="playlist.name"
+                class="max-w-xs m-2 mb-4"
+              >
+                <a :alt="playlist.name" :href="getPlaylistLink(playlist.name)">
+                  <Playlist :playlist="playlist" />
+                </a>
+              </li>
+            </ul>
+            <div v-if="playlistItems !== undefined && playlistItems.length > 0">
+              <ul class="list-none">
+                <li
+                  v-for="playlistItem in playlistItems"
+                  :key="playlistItem.name"
+                  class="border-b border-gray-800 first:border-t hover:bg-gray-800"
+                >
+                  <PlaylistItem
+                    :playlist-item="playlistItem"
+                    :set-source-function="setSource"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div
+            v-if="
+              !(playlists !== undefined && playlists.length > 0) &&
+              !(playlistItems !== undefined && playlistItems.length > 0)
+            "
+            class="text-center"
+          >
+            No items found.
+          </div>
+        </div>
+      </div>
+      <vue-plyr
+        v-if="playlistItems !== undefined"
+        ref="plyr"
+        class="fixed bottom-0 left-0 right-0"
+      >
+        <audio />
+      </vue-plyr>
     </section>
+    <!-- player below is for spacing (invisible) -->
+    <vue-plyr v-if="playlistItems !== undefined" class="invisible">
+      <audio />
+    </vue-plyr>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import get from 'lodash.get'
+import merge from 'lodash.mergewith'
+import { Component, Vue } from 'nuxt-property-decorator'
 
-export default Vue.extend({})
+import Playlist from '~/components/Playlist.vue'
+import PlaylistItem from '~/components/PlaylistItem.vue'
+
+interface AsyncData {
+  playlists?: object
+  playlistItems?: object
+}
+
+interface AxiosPlaylistData {
+  playlists: object
+  nextContinuationToken: string
+}
+
+@Component({
+  components: {
+    Playlist,
+    PlaylistItem,
+  },
+})
+export default class extends Vue {
+  playlists?: Array<object>
+
+  get player() {
+    return (this.$refs.plyr as any).player
+  }
+
+  async asyncData({
+    $axios,
+    query,
+  }: {
+    $axios: any
+    query: any
+  }): Promise<AsyncData> {
+    let continuationToken
+    const playlistsObject: object = {}
+
+    try {
+      do {
+        const playlistData: AxiosPlaylistData = await $axios.$get(
+          '/playlists',
+          {
+            params: new URLSearchParams(
+              merge(
+                {},
+                {
+                  ...(continuationToken !== undefined && {
+                    'continuation-token': continuationToken,
+                  }),
+                  ...(query.playlist !== undefined && {
+                    prefix: query.playlist,
+                  }),
+                }
+              )
+            ),
+          }
+        )
+
+        merge(
+          playlistsObject,
+          playlistData.playlists /* flattenPlaylists(playlistData.playlists) */
+        )
+        continuationToken = playlistData.nextContinuationToken
+      } while (continuationToken !== undefined)
+    } catch (e) {
+      return {
+        playlists: undefined,
+        playlistItems: undefined,
+      }
+    }
+
+    const playlists = []
+    const playlistKeyParts =
+      query.playlist !== undefined
+        ? query.playlist.split('/').join('/collections/').split('/')
+        : undefined
+    const playlistsSource =
+      query.playlist !== undefined
+        ? get(playlistsObject, playlistKeyParts, { collections: {} })
+            .collections
+        : playlistsObject
+    const playlistItems =
+      query.playlist !== undefined
+        ? get(playlistsObject, playlistKeyParts, { items: {} }).items
+        : playlistsObject
+
+    if (playlistsSource !== undefined) {
+      for (const [key, value] of Object.entries(playlistsSource)) {
+        playlists.push({ name: key, items: value })
+      }
+    }
+
+    return {
+      playlists,
+      playlistItems,
+    }
+  }
+
+  getPlaylistLink(name: string) {
+    const queryObject = JSON.parse(JSON.stringify(this.$route.query))
+    const playlistLinkParts: Array<string> = []
+
+    queryObject.playlist = encodeURIComponent(
+      [queryObject.playlist, name]
+        .filter(Boolean) // prevent initial join character
+        .join('/')
+    )
+
+    for (const [key, value] of Object.entries(queryObject)) {
+      playlistLinkParts.push(value === null ? key : `${key}=${value}`)
+    }
+
+    return `?${playlistLinkParts.join('&')}`
+  }
+
+  setSource(url: URL) {
+    this.player.config.controls = [
+      'play-large',
+      'play',
+      'progress',
+      'current-time',
+      'mute',
+      'volume',
+      'airplay',
+    ]
+    this.player.source = {
+      type: 'audio',
+      sources: [
+        {
+          src: url,
+          type: 'audio/mp3',
+        },
+      ],
+    }
+
+    this.player.play()
+  }
+}
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,26 @@ module.exports = {
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
+  variants: {
+    borderWidth: ['first'],
+  },
   plugins: [
     function ({ addBase, config }) {
       addBase({
         body: {
           background: config('theme.colors.gray.800'),
           color: config('theme.colors.white'),
+          h1: {
+            fontSize: config('theme.fontSize.6xl'),
+            fontWeight: config('theme.fontWeight.bold'),
+            marginBottom: config('theme.margin.4'),
+            textAlign: 'center',
+          },
+          h2: {
+            fontSize: config('theme.fontSize.4xl'),
+            fontWeight: config('theme.fontWeight.bold'),
+            marginBottom: config('theme.margin.3'),
+          },
         },
       })
     },
