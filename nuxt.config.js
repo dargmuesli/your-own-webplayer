570a6b342c2bea6a0ba19da87c0547214bded659
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-09-07 06:50:19 +0200

    Fill Index Page

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,9 @@ export default {
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseURL: 'http://localhost:3000/api/',
+  },
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
---
+++ vue-shim.d.ts
@@ -0,0 +1,3 @@
+declare module '*.vue' {
+  import axios from '@nuxtjs/axios'
+}
